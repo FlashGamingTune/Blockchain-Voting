84f67131996bdfcbd4b52753292733843fbfac0f
Saad Mohammed <saadmohammed.kv333@gmail.com>
2026-01-27 13:16:04 +0530

    Fix template folder path for Render

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(
     __name__,
     static_folder=os.path.join(os.path.dirname(__file__), "templates"),
-    static_folder="static"
+    static_folder="templates"
 )
 app.secret_key =  "secret123"
 
